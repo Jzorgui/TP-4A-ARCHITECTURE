235a8493085131283da4babae4e16df81fb0fce1
naim9432 <naim94600@gmail.com>
2020-01-28 11:40:22 +0100

    Initial Java Project

---
+++ tp-4A-2020/src/main/java/com/esiea/tp/4A/Calc.java
@@ -0,0 +1,12 @@
+package com.esiea.tp.4A;
+
+public class Calc {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+}
