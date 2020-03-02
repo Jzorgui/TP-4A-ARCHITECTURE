cf8156823bd55d4d7b8737469901b53c623b6844
Amine <amine_77500@hotmail.fr>
2020-03-02 22:33:06 +0100

    cleaning branch

---
+++ src/main/java/com/esiea/tp4A/domain/MarsRover.java
@@ -0,0 +1,22 @@
+package com.esiea.tp4A.domain;
+
+import com.esiea.tp4A.domain.Position.FixedPosition;
+
+public interface MarsRover {
+	default MarsRover initialize(Position position) {
+		return this;
+	}
+
+	default MarsRover updateMap(PlanetMap map) {
+		return this;
+	}
+
+	default MarsRover configureLaserRange(int range) {
+		return this;
+	}
+
+	default Position move(String command) {
+		return Position.of(0, 0, Direction.NORTH);
+	}
+	
+}
