6b2af0641215f49827d87ca9b36e4e42a9aabee6
Amine <amine_77500@hotmail.fr>
2020-03-09 14:01:21 +0100

    Fixing test

--- src/test/java/com/esiea/tp4A/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/MarsRoverTest.java
@@ -1,14 +1,9 @@
 package com.esiea.tp4A;
 
 import com.esiea.tp4A.domain.Direction;
-import com.esiea.tp4A.domain.MarsRover;
 import com.esiea.tp4A.domain.MarsRoverImp;
 import com.esiea.tp4A.domain.Position;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.util.Scanner;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,21 +11,16 @@ import org.junit.jupiter.api.Test;
 
 public class MarsRoverTest {
 
-	private MarsRoverImp marsRoverForward=new MarsRoverImp();
-
-
+	private MarsRoverImp marsRoverForward= new MarsRoverImp();
 	@Test
 	void moveForward() {
 		String command="f";
-		marsRoverForward.move(command);
 		
-		
-		System.out.println("   marsRoverForward   :\n x : "+marsRoverForward.getPosition().getX() +" y : "+ marsRoverForward.getPosition().getY()+" Direction : " 
-							+ marsRoverForward.getPosition().getDirection());
-		
-		
-		Assertions.assertThat(marsRoverForward.getPosition())
-			.isEqualTo(Position.of(0, 1, Direction.NORTH));
+		Assertions.assertThat(marsRoverForward.move(command))
+			.as("moving forward")
+			.extracting(Position::getX,Position::getY,Position::getDirection)
+			.containsExactly(0,1,Direction.NORTH);
 	}
+	
 }
  
