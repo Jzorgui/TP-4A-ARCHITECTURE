6fcfe9e13faf323839c67b3e6d46453e555213dd
Amine <amine_77500@hotmail.fr>
2020-04-16 20:33:29 +0200

    Cleaning project by deleting all unused web files and changing src/main/ressources to src/main/resources

--- src/main/java/com/esiea/tp4A/spring/SpringController.java
+++ src/main/java/com/esiea/tp4A/spring/SpringController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.esiea.tp4A.domain.LoadPlayer;
 import com.esiea.tp4A.domain.LocalMap;
