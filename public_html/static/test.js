3bca785a0fa9e8d25be4b98ebb89042142f21b3f
Amine <amine_77500@hotmail.fr>
2020-04-16 17:05:23 +0200

    folder for static files heroku

---
+++ public_html/static/test.js
@@ -0,0 +1,47 @@
+var jsoon = {
+    "player": {
+        "name": "lol",
+        "status": "alive",
+        "position": {
+            "x": 1,
+            "y": -2,
+            "direction": "NORTH"
+        },
+        "laser-range": 3
+    },
+    "localmap": {
+        "size": 26,
+        "obstacles": [{
+            "x": -3,
+            "y": 1
+        }, {
+            "x": 1,
+            "y": 3
+        },
+        {
+            "x": 1,
+            "y": 4
+        }],
+        "players": [
+          {
+              "direction": "SOUTH",
+              "name": "ptdl",
+              "x": 4,
+              "y": 3
+          },
+          {
+            "direction": "EAST",
+            "name": "ptdl",
+            "x": 1,
+            "y": 1
+        },
+        {
+            "direction": "WEST",
+            "name": "ptdl",
+            "x": 4,
+            "y": 4
+        }
+        
+        ]
+    }
+}
