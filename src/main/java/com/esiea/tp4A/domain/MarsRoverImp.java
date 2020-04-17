790e95f302fd46085f4e47f71d3d95ef6e7e6911
Jzorgui <37659681+Jzorgui@users.noreply.github.com>
2020-04-18 01:34:12 +0200

    clean MarsRoverImp.java

--- src/main/java/com/esiea/tp4A/domain/MarsRoverImp.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverImp.java
@@ -4,9 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MarsRoverImp implements MarsRover {
-
-	// Attribute
-	private String name;
+	/* Attribute */private String name;
 	private int lazerRange = 0;
 	private boolean status = true;
 	private int x, y;
@@ -15,92 +13,27 @@ public class MarsRoverImp implements MarsRover {
 	private LocalMap localMap;
 	private final MarsRoverMove roverMove = new MarsRoverMove();
 	private final MarsRoverFunction roverFunc = new MarsRoverFunction();
-	
-	// MapRange
-	private int xMaxMap, yMaxMap, xMinMap, yMinMap;
-
-	// Constructors
-	public MarsRoverImp(String name) {
-		this.name = name;
-	}
-
-	public MarsRoverImp() {
-	}
-
-	// Getters and Setters
-
-	public void setDirection(Direction direction) {
-		this.direction=direction;
-	}
-	
-	public void setLocalMap(LocalMap localMap) {
-		this.localMap = localMap;
-	}
-
-	public LocalMap getLocalMap() {
-		return this.localMap;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public Position getPosition() {
-		return position;
-	}
-
-	public void setPosition(Position pos) {
-		this.position = pos;
-	}
-
-	public int getLazerRange() {
-		return lazerRange;
-	}
-
-	public boolean getStatus() {
-		return status;
-	}
-
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public int getxMaxMap() {
-		return xMaxMap;
-	}
-
-	public int getyMaxMap() {
-		return yMaxMap;
-	}
-
-	public int getxMinMap() {
-		return xMinMap;
-	}
-
-	public int getyMinMap() {
-		return yMinMap;
-	}
-
+	/* MapRange */private int xMaxMap, yMaxMap, xMinMap, yMinMap;
+	/* Constructors */public MarsRoverImp(String name) { this.name = name; }
+	public MarsRoverImp() { }
+	/* Getters and Setters */public void setDirection(Direction direction) { this.direction=direction; }
+	public void setLocalMap(LocalMap localMap) { this.localMap = localMap; }
+	public LocalMap getLocalMap() { return this.localMap; }
+	public void setX(int x) { this.x = x; }
+	public void setY(int y) { this.y = y; }
+	public void setName(String name) { this.name = name; }
+	public String getName() { return name; }
+	public Position getPosition() { return position; }
+	public void setPosition(Position pos) { this.position = pos; }
+	public int getLazerRange() { return lazerRange; }
+	public boolean getStatus() { return status; }
+	public void setStatus(boolean status) { this.status = status; }
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public int getxMaxMap() { return xMaxMap; }
+	public int getyMaxMap() { return yMaxMap; }
+	public int getxMinMap() { return xMinMap; }
+	public int getyMinMap() { return yMinMap; }
 	// Functions
 	@Override
 	public Position move(String command) {
@@ -113,7 +46,6 @@ public class MarsRoverImp implements MarsRover {
 		case "b":
 			roverMove.moveBack(direction, this);
 			break;
-
 		case "l":
 			roverMove.turnLeft(direction, this);
 			break;
@@ -133,45 +65,36 @@ public class MarsRoverImp implements MarsRover {
 			y = initialY;
 		}
 		position = Position.of(x, y, direction);
-
 		return position;
 	}
-
 	public void GenerateMap(int mapSize) {
 		xMaxMap = mapSize / 2;
 		xMinMap = xMaxMap - mapSize;
 		yMaxMap = mapSize / 2;
 		yMinMap = yMaxMap - mapSize;
 	}
-
 	public JSONArray radarObstacle(LocalMap localMap2) {
 		JSONArray mapRadar = new JSONArray();
 		for (int i = -16; i < x + 16; i++) {
 			for (int p = y - 16; p < y + 16; p++) {
 				for (Position pos : localMap2.getSetPos()) {
-					if (pos.getX() == i && pos.getY() == p) {
-						mapRadar.put(new JSONObject().put("x", pos.getX()).put("y", pos.getY()));
-					}
+					if (pos.getX() == i && pos.getY() == p) { mapRadar.put(new JSONObject().put("x", pos.getX()).put("y", pos.getY())); }
 				}
 			}
 		}
 		return mapRadar;
 	}
-
 	public JSONArray radarEnnemie(LocalMap localMap2) {
 		JSONArray mapRadar = new JSONArray();
 		for (int i = -16; i < x + 16; i++) {
 			for (int p = y - 16; p < y + 16; p++) {
 				for (MarsRoverImp rov : localMap2.getSetRover()) {
-					if (rov.getX() == i && rov.getY() == p && !rov.getName().equals(name)) {
-						mapRadar.put(new JSONObject().put("x", rov.getX()).put("y", rov.getY()).put("name", rov.getName()));
-					}
+					if (rov.getX() == i && rov.getY() == p && !rov.getName().equals(name)) { mapRadar.put(new JSONObject().put("x", rov.getX()).put("y", rov.getY()).put("name", rov.getName())); }
 				}
 			}
 		}
 		return mapRadar;
 	}
-
 	@Override
 	public MarsRover configureLaserRange(int range) {
 		lazerRange = range;
